25b941e77727b559b40d28ed68af0d3c6e5dbbaf
apet2929 <apet2929@csd99.org>
2020-03-25 15:47:18 -0500

    Working Nut Grinder TileEntity with inventory

--- src/main/java/com/MoonJew/nutt/blocks/NutGrinderTile.java
+++ src/main/java/com/MoonJew/nutt/blocks/NutGrinderTile.java
@@ -1,20 +1,77 @@
 package com.MoonJew.nutt.blocks;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import static com.MoonJew.nutt.blocks.ModBlocks.NUTGRINDER_TILE;
 
 public class NutGrinderTile extends TileEntity implements ITickableTileEntity {
+
+    private LazyOptional<IItemHandler> handler = LazyOptional.of(this::createHandler);
     public NutGrinderTile(){
         super(NUTGRINDER_TILE);
     }
 
     @Override
     public void tick() {
-        if(world.isRemote){
-            System.out.println("NutGrinderTile.tick()");
-        }
+    }
+
+    @Override
+    public void read(CompoundNBT tag) {
+        CompoundNBT invTag = tag.getCompound("inv");
+        handler.ifPresent(h -> ((INBTSerializable<CompoundNBT>)h).deserializeNBT(invTag));
+        super.read(tag);
+    }
+
+    @Override
+    public CompoundNBT write(CompoundNBT tag) {
+        handler.ifPresent(h -> {
+            CompoundNBT compound = ((INBTSerializable<CompoundNBT>)h).serializeNBT();
+            tag.put("inv", compound);
+        });
+
+        return super.write(tag);
+    }
+
+    private ItemStackHandler createHandler(){
+        return new ItemStackHandler(1) {
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                return stack.getItem() == Items.DIAMOND;
+            }
+
+            @Nonnull
+            @Override
+            public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+                if(stack.getItem() != Items.DIAMOND){
+                    return stack;
+                }
+                return super.insertItem(slot, stack, simulate);
+            }
+        };
     }
 
 
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY){
+            return handler.cast();
+        }
+        return super.getCapability(cap, side);
+    }
 }
